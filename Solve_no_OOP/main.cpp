80b174e19c0b8e02e64e4b322708c9daff0cb1c1
davxis1337 <screambvb1@gmail.com>
2025-09-11 01:03:19 +0300

    nothing

--- Solve_no_OOP/main.cpp
+++ Solve_no_OOP/main.cpp
@@ -1,7 +1,7 @@
 #include "equation.h"
 
 int main() {
-    
+    cout << "============= Solving quadratic equation =============" << endl;
     Coefficients coeffs = input_coefficients();
     solve_equation(coeffs.a, coeffs.b, coeffs.c);
     
